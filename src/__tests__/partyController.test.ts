030125568658b3759acfa5d8b99b143ec278a946
JMTiburcio <jmrtme@gmail.com>
2023-05-15 15:59:41 -0300

    test: adicionando testes para casos de erro

--- src/__tests__/partyController.test.ts
+++ src/__tests__/partyController.test.ts
@@ -9,26 +9,16 @@ jest.mock("../models/Beer");
 jest.mock("../utils/getToken");
 jest.mock("../utils/getPlaylist");
 
-// Crie um mock manual para o módulo 'partyController'
-jest.mock("../controllers/partyController", () => {
-  const originalModule = jest.requireActual("../controllers/partyController");
-
-  return {
-    ...originalModule,
-    getPlaylist: jest.fn(),
-  };
-});
-
 describe("handleBeerAndPlaylist", () => {
   beforeEach(() => {
     // Limpa os mocks antes de cada teste
     jest.clearAllMocks();
   });
 
-  it("deve retornar uma cerveja e uma playlist quando houver cervejas filtradas", async () => {
+  it("deve retornar primeira cerjeva em caso de empate", async () => {
     const mockedBeers = [
-      { beerStyle: "IPA", minTemp: -2, maxTemp: 3 },
-      { beerStyle: "Lager", minTemp: -1, maxTemp: 0 },
+      { beerStyle: "IPA", minTemp: -1, maxTemp: 3 },
+      { beerStyle: "Lager", minTemp: -1, maxTemp: 3 },
     ];
     const mockedToken = "mocked-token";
     const mockedPlaylist = {
@@ -41,19 +31,14 @@ describe("handleBeerAndPlaylist", () => {
     (getToken as jest.Mock).mockResolvedValue(mockedToken);
     (getPlaylist as jest.Mock).mockResolvedValue(mockedPlaylist);
 
-    const req = { body: { temperature: 10 } } as Request;
+    const req = { body: { temperature: 2 } } as Request;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const res: any = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
 
-    try {
-      await handleBeerAndPlaylist(req, res);
-      console.log("OK!");
-    } catch (error) {
-      console.error("Error in handleBeerAndPlaylist:", error);
-    }
+    await handleBeerAndPlaylist(req, res);
 
     expect(Beer.find).toHaveBeenCalledWith({
       minTemp: { $lte: req.body.temperature },
@@ -68,6 +53,117 @@ describe("handleBeerAndPlaylist", () => {
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({
       beerStyle: mockedBeers[0].beerStyle,
+      playlist: mockedPlaylist,
+    });
+  });
+
+  it("deve retornar um erro 404 quando não houver cervejas filtradas", async () => {
+    (Beer.find as jest.Mock).mockImplementation(() => ({
+      sort: () => Promise.resolve([]),
+    }));
+
+    const req = { body: { temperature: 10 } } as Request;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const res: any = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await handleBeerAndPlaylist(req, res);
+
+    expect(Beer.find).toHaveBeenCalledWith({
+      minTemp: { $lte: req.body.temperature },
+      maxTemp: { $gte: req.body.temperature },
+    });
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      error: "Nenhuma cerveja encontrada",
+    });
+  });
+
+  it("deve retornar um erro 500 quando houver uma falha em getToken", async () => {
+    const mockedBeers = [{ beerStyle: "IPA", minTemp: -2, maxTemp: 3 }];
+    (Beer.find as jest.Mock).mockImplementation(() => ({
+      sort: () => Promise.resolve(mockedBeers),
+    }));
+    (getToken as jest.Mock).mockRejectedValue(new Error("Token error"));
+    // Similarmente, você pode mockar getPlaylist para falhar.
+
+    const req = { body: { temperature: 1 } } as Request;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const res: any = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await handleBeerAndPlaylist(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: "Erro ao buscar cerveja" });
+  });
+
+  it("deve retornar um erro 404 quando não encontrar playlist", async () => {
+    const mockedBeers = [{ beerStyle: "IPA", minTemp: -2, maxTemp: 3 }];
+    const mockedToken = "mocked-token";
+
+    (Beer.find as jest.Mock).mockImplementation(() => ({
+      sort: () => Promise.resolve(mockedBeers),
+    }));
+    (getToken as jest.Mock).mockResolvedValue(mockedToken);
+    (getPlaylist as jest.Mock).mockResolvedValue(null);
+
+    const req = { body: { temperature: 1 } } as Request;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const res: any = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await handleBeerAndPlaylist(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      error: `Nenhuma playlist encontrada para: ${mockedBeers[0].beerStyle}`,
+    });
+  });
+
+  it("deve retornar a cerveja mais próxima quando houver múltiplas cervejas", async () => {
+    const mockedBeers = [
+      { beerStyle: "Longe1", minTemp: -1, maxTemp: 4 },
+      { beerStyle: "Próxima", minTemp: -1, maxTemp: 3 },
+      { beerStyle: "Longe2", minTemp: -2, maxTemp: 4 },
+      { beerStyle: "Longe3", minTemp: 0, maxTemp: 5 },
+    ];
+    const mockedToken = "mocked-token";
+    const mockedPlaylist = {
+      name: "Test Playlist",
+      tracks: [{ name: "Track 1", artist: "Artist 1", link: "link1" }],
+    };
+    (Beer.find as jest.Mock).mockImplementation(() => ({
+      sort: () => Promise.resolve(mockedBeers),
+    }));
+    (getToken as jest.Mock).mockResolvedValue(mockedToken);
+    (getPlaylist as jest.Mock).mockResolvedValue(mockedPlaylist);
+
+    const req = { body: { temperature: 1 } } as Request;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const res: any = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await handleBeerAndPlaylist(req, res);
+
+    expect(Beer.find).toHaveBeenCalledWith({
+      minTemp: { $lte: req.body.temperature },
+      maxTemp: { $gte: req.body.temperature },
+    });
+    expect(getToken).toHaveBeenCalled();
+    expect(getPlaylist).toHaveBeenCalledWith(mockedToken, "Próxima");
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      beerStyle: "Próxima",
       playlist: mockedPlaylist,
     });
   });
